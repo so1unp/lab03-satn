74a82fb854dc6441b027543e7593000d32b8f4e9
AverageZeko <lizandro.e.marino@gmail.com>
2026-04-25 00:54:36 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA no se si github tiene limite de caracteresAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- forkprio.c
+++ forkprio.c
@@ -1,81 +1,69 @@
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/times.h>      // times()
-#include <unistd.h> // getpid()
-#include <sys/wait.h> // wait()
+#include <sys/times.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <signal.h>
 #include <string.h>
 #include <sys/resource.h>
 
+#define NUMB_OF_ARGUMENTS 4
 
-#define NUMB_OF_ARGUMENTS 3
-
-
-
-int busywork(void)
-{
+int busywork(void) {
     struct tms buf;
     for (;;) {
         times(&buf);
     }
 }
 
-void sig_handler(int p) {
-    //printf("Soy el hijo con PID %d y me acaban de matar. rip\n", getpid());
-    //printf("%d: %s\n", p, strsignal(p));
+void sig_handler() {
     struct rusage usage;
     getrusage(RUSAGE_SELF, &usage);
-    //printf("\nChild %d (nice %2d):\t%3li\n", getpid(), getpriority(PRIO_PROCESS,getpid()), usage->ru_stime);
-    printf("\nChild %d \t%3li segundos?\n", getpid(), usage.ru_stime.tv_sec);
+    printf("Child %d (nice %2d):\t%1li seconds\n", (id_t)getpid(), getpriority(PRIO_PROCESS, (id_t) getpid()), usage.ru_stime.tv_sec);
     exit(EXIT_SUCCESS);
 }
 
-
-
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     if(argc != NUMB_OF_ARGUMENTS){
-        fprintf(stderr,"incorrect number of arguments");
+        fprintf(stderr,"Numero incorrecto de argumentos\n");
         exit(-1);
     }
     
-    int childs = atoi(argv[1]);
-    unsigned time = atoi(argv[2]);
-    //int priority = atoi(argv[3]);
+    int amountChilds = atoi(argv[1]);
+    unsigned time = (unsigned) atoi(argv[2]);
+    int priority = atoi(argv[3]);
     
-    pid_t hijos[childs];
+    pid_t children[amountChilds];
     
-    
-    for(int i = 0; i < childs; i++){
-        pid_t h;
-        if((h = fork()) == 0){
+    for(int i = 0; i < amountChilds; i++){
+        pid_t childPid;
+        if((childPid = fork()) == 0){
             struct sigaction sigAct;
             sigAct.sa_handler = sig_handler;
             sigemptyset(&sigAct.sa_mask);
-            //sigAct.sa_flags = SA_RESTART;
             if (sigaction(SIGTERM, &sigAct, NULL) == -1) {
                 perror("sigaction");
                 return 1;
             }
-            printf("Hijo con PID: %d se va a perder tiempo\n", getpid());
+
             busywork();
         }
-        hijos[i] = h;
+        if (priority == 1) {
+            setpriority(PRIO_PROCESS, (id_t)childPid, i+1);
+        }
+        children[i] = childPid;
     }
     
     printf("Proceso padre se va a dormir\n");
-    sleep(time);
-
-    for (int j = 0; j < childs; j++) {
-        printf("Hijo numero %d, PID: %d\n", j+1, hijos[j]);
+    if (time == 0) {
+        busywork();
+    }   else {
+        sleep(time);
     }
-    //mandar señales
-    for(int i=0;i<childs;i++){
 
-        //printf("kill proceso %d\n",hijos[i]);
-        kill(hijos[i],SIGTERM);
+    for(int i = 0;i < amountChilds; i++){
+        kill(children[i],SIGTERM);
     }
     exit(EXIT_SUCCESS);
 }
